468cfea9f372056db2a143b43a91e050da6c2eb4
liefeld <liefeld@broadinstitute.org>
2026-04-23 08:11:17 -0700

    remove old partial tests

--- test2.py
+++
@@ -1,5 +0,0 @@
-import sys; 
-sys.path.insert(0, 'runner'); 
-sys.path.insert(0, '.')
-from tools import parse_gp_module
-print(parse_gp_module('/Users/liefeld/GenePattern/modules/tfsites.GenerateMotifDatabase/build/tfsites.GenerateMotifDatabase.zip'))
